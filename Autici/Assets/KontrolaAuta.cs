950d89c2f49f867348c2d643fa538741fe55716d
LauraTkalcec <tkalcec.laura@gmail.com>
2020-05-13 14:43:41 +0200

    --

---
+++ Autici/Assets/KontrolaAuta.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KontrolaAuta : MonoBehaviour {
+
+    public float carSpeed;
+    public float MaxPos = 2.3f;
+    Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+        position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+        position.x = Mathf.Clamp(position.x, -2.4f, 2.4f);
+        transform.position = position;
+        position.y += Input.GetAxis("Vertical") * carSpeed * Time.deltaTime;
+        position.y = Mathf.Clamp(position.y, -4f, 3.2f);
+        transform.position = position;
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "ProtivnickiAuto")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
